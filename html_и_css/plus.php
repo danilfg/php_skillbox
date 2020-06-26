87d594f0785b57b5a857aabb5de3f27ac556f4cc
danilfg <danilfg@yahoo.com>
2020-06-26 15:15:25 +0300

    done plus, minus, division, multiply

---
+++ html_и_css/plus.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Мини - сайт, математика чисел 5 и </title>
+</head>
+<body>
+
+<h1>Сложение чисел 5 и 3</h1>
+<p>5 + 3 = <?=5 + 3?></p>
+<a href="/">Назад</a>
+
+</body>
+</html>
