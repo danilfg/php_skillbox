dabcca8eb914df15af07d787aa83086b3297bc73
danilfg <danilfg@yahoo.com>
2020-06-26 17:16:02 +0300

    done module 2

--- css/index.php
+++ css/index.php
@@ -10,8 +10,18 @@
     <p id="paragraph">Абзац 2</p>
     <p class="red">Абзац 3</p>
     <p data-color="blue">Абзац 4</p>
-    <p>Абзац 5</p>
-    <p>Абзац 6</p>
+    <p class="red white">Абзац 5</p>
+    <p style="background-color: yellow">Абзац 6</p>
 
+    <div class="red">Текст в теге div</div>
+    <div class="gray white">Текст в теге div</div>
+
+    <div class="menu">
+        <a href="#">Ссылка в div</a>
+        <ul>
+            <li><a href="#">Ссылка в списке 1</a></li>
+            <li><a href="#">Ссылка в списке 2</a></li>
+    </ul></div>
 </body>
 </html>
+ 
